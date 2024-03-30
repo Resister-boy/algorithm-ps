9dfbdac9cdef52a20553fb5779eefc5ccd030231
supersquad <supersquad@supersquadui-MacBookPro.local>
2024-03-30 16:52:13 +0900

    solved: 2557

--- golang/1000/main.go
+++ golang/1000/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    fmt
-    bufio
-    os
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
-    
-    defer writer.Flush()
 
-    var a, b int
-    
-    fmt.Fscanln(reader, &a, &b)
-    
-    fmt.Fprintln(writer, a + b);
+	defer writer.Flush()
+
+	var a, b int
+
+	fmt.Fscanln(reader, &a, &b)
+
+	fmt.Fprintln(writer, a+b)
 }
