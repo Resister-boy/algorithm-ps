d2e22eb52837c34f5380f86802408d7b9f723826
supersquad <supersquad@supersquadui-MacBookPro.local>
2024-03-31 12:08:29 +0900

    solved: 11382

---
+++ golang/11382/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+    
+    var a, b, c int
+    
+    fmt.Fscanln(reader, &a, &b, &c)
+    fmt.Fprintln(writer, a + b + c)
+}
