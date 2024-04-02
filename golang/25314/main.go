ab3acfa7029009139c718e94a3e54fbe95e5119d
resister-boy <hassanpumped17@gmail.com>
2024-04-02 13:54:36 +0900

    solved: 25314

---
+++ golang/25314/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var a int
+
+	fmt.Fscanln(reader, &a)
+	for i := 0; i < (a / 4); i++ {
+		fmt.Fprintf(writer, "long ")
+	}
+	fmt.Fprintln(writer, "int")
+}
