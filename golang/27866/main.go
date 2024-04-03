9190da8f07b94b7622a84deedb4fc6ec5150914d
resister-boy <hassanpumped17@gmail.com>
2024-04-03 21:24:03 +0900

    solved: 27866

---
+++ golang/27866/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var s string
+	var a int
+
+	fmt.Fscanln(reader, &s)
+	fmt.Fscanln(reader, &a)
+	fmt.Fprintf(writer, "%c", s[a-1])
+}
